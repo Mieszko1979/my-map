3af27bf8ab58560983bee69b2f908d7eb7a9602f
Mieszko1979 <1377869@gmail.com>
2026-03-30 22:26:01 +0300

    Update points.js

--- points.js
+++ points.js
@@ -3,21 +3,18 @@ var myPoints = [
     lat: 55.7512,
     lng: 37.6184,
     title: "День 1: Москва",
-    desc: "Старт маршрута",
-    link: "1.gpx" 
+    desc: "Старт маршрута. Погода +25.",
+    link: "1.gpx",              // Файл в папке tracks
+    ext_link: "https://www.strava.com/activities/12345", // Ссылка на страву
+    ext_title: "Strava"         // Название кнопки
   },
   {
     lat: 55.8500,
     lng: 38.4500,
-    title: "День 2: Область",
-    desc: "Ночевка в лесу",
-    link: "2.gpx" 
-  },
-  {
-    lat: 56.8500,
-    lng: 39.4500,
-    title: "День 3: Область",
-    desc: "Ночевка в лесу",
-    link: "track.gpx" 
+    title: "День 2: Ночевка",
+    desc: "Проехал 80 км. Устал.",
+    link: "2.gpx",
+    ext_link: "https://www.komoot.com/tour/67890", // Ссылка на комут
+    ext_title: "Komoot"
   }
 ];
