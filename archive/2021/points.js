feef0f3d46b7cb8c6d05536ef0a57607eef115cd
Mieszko1979 <1377869@gmail.com>
2026-04-02 13:48:59 +0300

    Update points.js

--- archive/2021/points.js
+++ archive/2021/points.js
@@ -4,7 +4,9 @@ var points2021 = [
   { day: "3", lat: 56.538892, lng: 50.246028, title: "26.05.2021", desc: "Описание", link: "", vk: "" },
   { day: "4", lat: 56.228895, lng: 51.13198, title: "27.05.2021", desc: "Описание", link: "27052021.gpx", vk: "" },
   { day: "5", lat: 56.187957, lng: 51.895589, title: "28.05.2021", desc: "Описание", link: "28052021.gpx", vk: "https://vk.com/wall-171082824_1063" },
-  { day: "6", lat: 56.980734, lng: 48.932924, title: "29.05.2021", desc: "Описание", link: "29052021.gpx", vk: "https://vk.com/wall-171082824_1066" },
-  { day: "7", lat: 56.980734, lng: 48.932924, title: "30.05.2021", desc: "Описание", link: "30052021.gpx", vk: "https://vk.com/wall-171082824_1070" },
+  { day: "6", lat: 56.552535, lng: 52.951987, title: "29.05.2021", desc: "Описание", link: "29052021.gpx", vk: "https://vk.com/wall-171082824_1066" },
+  { day: "7", lat: 56.813463, lng: 53.758479, title: "30.05.2021", desc: "Описание", link: "30052021.gpx", vk: "https://vk.com/wall-171082824_1070" },
+  { day: "8", lat: 56.751697, lng: 54.9824940, title: "31.05.2021", desc: "Описание", link: "31052021.gpx", vk: "https://vk.com/wall-171082824_1074" },
+  { day: "9", lat: 56.5344400, lng: 56.0959730, title: "01.06.2021", desc: "Описание", link: "01062021.gpx", vk: "https://vk.com/wall-171082824_1074" },
   
 ];
