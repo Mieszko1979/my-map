d8c363244c0dd972f3c13988318b621f49931629
Mieszko1979 <1377869@gmail.com>
2026-04-03 15:36:50 +0300

    Update points.js

--- archive/2019/points.js
+++ archive/2019/points.js
@@ -1,3 +1,21 @@
 var points2019 = [
-  { day: "1", lat: 55.75, lng: 37.61, title: "Путешествие 2019: День 1", desc: "Описание", link: "2019.gpx", vk: "" },
+  { day: "1", lat: 56.942146, lng: 47.870633, title: "05.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_471" },
+  { day: "2", lat: 56.133693, lng: 48.33075, title: "06.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_471" },
+  { day: "3", lat: 55.707742, lng: 49.216314, title: "07.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_471" },
+  { day: "4", lat: 55.378943, lng: 49.936024, title: "08.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_483" },
+  { day: "5", lat: 55.240978, lng: 50.833483, title: "09.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_491" },
+  { day: "6", lat: 54.9044, lng: 52.3154, title: "10.08.2019", desc: "Warmshowers", link: "", vk: "https://vk.com/wall-171082824_493" },
+  { day: "7", lat: 54.609583, lng: 52.685532, title: "11.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_497" },
+  { day: "8", lat: 54.316426, lng: 53.392542, title: "12.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_502" },
+  { day: "9", lat: 53.631291, lng: 53.638224, title: "13.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_503" },
+  { day: "10", lat: 53.10948, lng: 54.358629, title: "14.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_512" },
+  { day: "11", lat: 52.687271, lng: 54.898243, title: "15.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_518" },
+  { day: "12", lat: 52.062636, lng: 55.258504, title: "16.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_521" },
+  { day: "13-14", lat: 51.763094, lng: 55.091821, title: "17.08.2019 - 18.08.2019", desc: "Хостел", link: "", vk: "https://vk.com/wall-171082824_522" },
+  { day: "15", lat: 51.276754, lng: 54.988114, title: "19.08.2019", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_528" },
+  { day: "3", lat: 55.707742, lng: 49.216314, title: "07.08.2019", desc: "Палатка", link: "", vk: "" },
+
+  
+
+  { day: "3", lat: 55.707742, lng: 49.216314, title: "07.08.2019", desc: "Палатка", link: "", vk: "" },
 ];
