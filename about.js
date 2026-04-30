6496f323bb3e73a8434eae6cb7766e34f1c9ec04
Mieszko1979 <1377869@gmail.com>
2026-04-30 21:52:48 +0300

    Update about.js

--- about.js
+++ about.js
@@ -5,7 +5,7 @@ const aboutData = {
     // ДОБАВЛЯЙ ЛЮБЫЕ СОЦСЕТИ ТУТ
     socials: [
         { id: 'vk', url: "https://vk.com/..." },
-        { id: 'dzen', url: "https://dzen.ru/..." },
+        { id: 'Дзен', url: "https://dzen.ru/..." },
         { id: 'youtube', url: "https://youtube.com/..." },
         { id: 'telegram', url: "https://t.me/..." },
         { id: 'instagram', url: "" }, // Если пусто, иконка не появится
