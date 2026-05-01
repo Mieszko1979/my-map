2b647396de7355693bdad0122c91c61da6e8ca68
Mieszko1979 <1377869@gmail.com>
2026-05-01 15:51:29 +0300

    Update about.js

--- about.js
+++ about.js
@@ -2,34 +2,35 @@ const aboutData = {
     name: "Mieszko",
     bio: "Велопутешественник. Исследую мир на двух колесах. В этом архиве — все мои маршруты и приключения.",
     
-    // ДОБАВЛЯЙ ЛЮБЫЕ СОЦСЕТИ ТУТ
+    // СОЦИАЛЬНЫЕ СЕТИ
     socials: [
         { id: 'vk', url: "https://vk.com/mieszko_travel" },
-        { id: 'Dzen', url: "https://dzen.ru/mieszko_travel" },
-        { id: 'youtube', url: "https://youtube.com/..." },
-        { id: 'telegram', url: "https://t.me/..." },
-        { id: 'instagram', url: "" }, // Если пусто, иконка не появится
-        { id: 'odnoklassniki', url: "https://ok.ru/group/54355827359920" },
-        { id: 'strava', url: "https://strava.com/..." },
-        { id: 'komoot', url: "https://komoot.com/..." }
+        { id: 'dzen', url: "https://dzen.ru/mieszko_travel" },
+        { id: 'odnoklassniki', url: "https://ok.ru/mieszkotravel" },
+        { id: 'youtube', url: "https://www.youtube.com/@map9" },
+        { id: 'telegram', url: "https://t.me/mieszko_travel" },
+        { id: 'instagram', url: "https://www.instagram.com/mieszko_9/" },
+        { id: 'strava', url: "https://www.strava.com/athletes/mieszko9" },
+        { id: 'komoot', url: "https://www.komoot.com/user/2104612336027" }
     ],
 
     support: {
         // КУАР-КОДЫ (Для ПК версии слева)
         qrCodes: [
-            { name: "Сбербанк", img: "qr_sber.png" },
-            { name: "Т-Банк", img: "qr_tbank.png" }
+            { name: "CloudTips (Т-Банк)", img: "CloudTips.png" },
+            { name: "OZON Банк", img: "Qrozon.png" }
         ],
-        // СПОСОБЫ ОПЛАТЫ (Для ПК версии справа, для мобилок - списком)
-        // Добавляй любые: donationalerts, donatpay, boosty и т.д.
+        // КНОПКИ ОПЛАТЫ (Для ПК версии справа, для мобилок - списком)
         buttons: [
-            { name: "Т-Банк (CloudTips)", url: "https://pay.cloudtips.ru/p/977f6357", color: "#ffdd00", textColor: "#000" },
-            { name: "ЮMoney", url: "https://yoomoney.ru/to/410014509615752", color: "#803cff", textColor: "#fff" },
-            { name: "Donationalerts", url: "https://donationalerts.com/...", color: "#ffad23", textColor: "#fff" },
-            { name: "DonatPay", url: "https://donatpay.ru/...", color: "#673ab7", textColor: "#fff" },
-            { name: "VK Pay", url: "", color: "#0077ff", textColor: "#fff" },
-            { name: "Boosty", url: "", color: "#f15f2c", textColor: "#fff" },
-            { name: "Дзен", url: "https://dzen.ru/mieszko_travel?donate=true", color: "#0077ff", textColor: "#fff" },
+            { name: "CloudTips (Т-Банк)", url: "https://pay.cloudtips.ru/p/977f6357", color: "#FFDD00", textColor: "#000" },
+            { name: "ЮMoney", url: "https://yoomoney.ru/to/410014509615752", color: "#803CFF", textColor: "#fff" },
+            { name: "Ozon Банк", url: "https://finance.ozon.ru/apps/sbp/ozonbankpay/019ddf2d-15f0-702f-93c2-e82fcba8fb1b", color: "#005BFF", textColor: "#fff" },
+            { name: "Дзен донаты", url: "https://dzen.ru/mieszko_travel?donate=true", color: "#000", textColor: "#fff" },
+            { name: "VK Pay", url: "", color: "#0077FF", textColor: "#fff" },
+            { name: "Donationalerts", url: "", color: "#FFAD23", textColor: "#fff" },
+            { name: "DonatPay", url: "", color: "#673AB7", textColor: "#fff" },
+            { name: "Boosty", url: "", color: "#F15F2C", textColor: "#fff" },
+            { name: "Alipay", url: "", color: "#00A3EE", textColor: "#fff" }
         ]
     }
 };
