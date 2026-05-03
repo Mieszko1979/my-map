808aef086f690befb5023b5fbaee4a2a92c4526d
Mieszko1979 <1377869@gmail.com>
2026-05-03 19:40:43 +0300

    Update about.js
    добавлено фото и крипта

--- about.js
+++ about.js
@@ -1,6 +1,8 @@
 const aboutData = {
-    name: "Мое велопутешествие",
-    bio: "Меня зовут Михаил. Это карта моего велопутешествия (если я в пути). Также здесь собраны все мои предыдущие велопутешествия. После нажатия на кружок откроется окошко, там будет небольшпя информация от ночевке. Также будет ссылка на пост в ВК, и трек за текущий день (конечно если он будет записан).",
+    name: "Михаил (Mieszko)",
+    userPhoto: "my_photo.jpg", // Твое фото (левая панель)
+    bikePhoto: "bike_photo.jpg", // Фото велосипеда (правая панель)
+    bio: "Меня зовут Михаил. Это карта моего велопутешествия (если я в пути). Также здесь собраны все мои предыдущие велопутешествия. После нажатия на кружок откроется окошко, там будет небольшая информация от ночевке. Также будет ссылка на пост в ВК, и трек за текущий день (конечно если он будет записан).",
     
     // СОЦИАЛЬНЫЕ СЕТИ
     socials: [
@@ -20,17 +22,22 @@ const aboutData = {
             { name: "CloudTips (Т-Банк)", img: "CloudTips.png" },
             { name: "OZON Банк", img: "Qrozon.png" }
         ],
-        // КНОПКИ ОПЛАТЫ (Для ПК версии справа, для мобилок - списком)
+        // СПОСОБЫ ОПЛАТЫ (Для ПК версии справа, для мобилок - списком)
         buttons: [
             { name: "CloudTips (Т-Банк)", url: "https://pay.cloudtips.ru/p/977f6357", color: "#FFDD00", textColor: "#000" },
             { name: "ЮMoney", url: "https://yoomoney.ru/to/410014509615752", color: "#803CFF", textColor: "#fff" },
             { name: "Ozon Банк", url: "https://finance.ozon.ru/apps/sbp/ozonbankpay/019ddf2d-15f0-702f-93c2-e82fcba8fb1b", color: "#005BFF", textColor: "#fff" },
             { name: "Дзен донаты", url: "https://dzen.ru/mieszko_travel?donate=true", color: "#000", textColor: "#fff" },
-            { name: "VK Pay", url: "https://vk.com/mieszko_travel?w=app5727453_-171082824", color: "#0077FF", textColor: "#fff" },
-            { name: "Donationalerts", url: "https://www.donationalerts.com/r/mieszko", color: "#FFAD23", textColor: "#fff" },
+            { name: "VK Pay", url: "", color: "#0077FF", textColor: "#fff" },
+            { name: "Donationalerts", url: "", color: "#FFAD23", textColor: "#fff" },
             { name: "DonatPay", url: "", color: "#673AB7", textColor: "#fff" },
             { name: "Boosty", url: "", color: "#F15F2C", textColor: "#fff" },
-            { name: "Alipay", url: "https://www.", color: "#00A3EE", textColor: "#fff" }
+            { name: "Alipay", url: "", color: "#00A3EE", textColor: "#fff" }
+        ],
+        // КРИПТОВАЛЮТА (Копирование адреса)
+        crypto: [
+            { name: "USDT (TRC-20)", address: "TYourAddressHere...", color: "#26A17B" },
+            { name: "USDC (BEP-20)", address: "0xYourAddressHere...", color: "#2775CA" }
         ]
     }
 };
