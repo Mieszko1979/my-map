26a7646612283f778dd390356dd782b83c22cca6
Mieszko1979 <1377869@gmail.com>
2026-05-12 18:29:01 +0300

    Update points.js

--- points.js
+++ points.js
@@ -1,20 +1,6 @@
 var myPoints = [
-  {
-    day: "1",
-    lat: 55.7512,
-    lng: 37.6184,
-    title: "01.02.2026",
-    desc: "Палатка",
-    link: "2.gpx",
-    vk: "https://vk.com/mieszko_travel"
-  },
-  {
-    day: "2-4",
-    lat: 55.8500,
-    lng: 38.4500,
-    title: "22.03.2026-23.02.2026",
-    desc: "Тут мы стояли 3 дня лагерем.",
-    link: "4.gpx",
-    vk: "https://vk.com/wall123_457"
-  }
+  { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", desc: "Палатка", link: "", vk: "" },
+  { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", desc: "Палатка", link: "", vk: "https://vk.com/wall-171082824_1252" },
+  { day: "", lat: , lng: , title: "01.07.2024", desc: "Палатка", link: "", vk: "" }
+ 
 ];
