89c7befa00078476aad1b300303107a3c3bd0ff3
Mieszko1979 <1377869@gmail.com>
2026-05-29 13:05:32 +0300

    Update points.js

--- points.js
+++ points.js
@@ -1,7 +1,7 @@
 var myPoints = [
   { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 124.5, desc: "Палатка", link: "3.gpx", vk: "" },
   { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 85, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
-  { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 100, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
+  { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 100, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
   
  
 ];
