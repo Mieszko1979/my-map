c138da67ab4f60f58aad95dcb2e66835de6a5b62
Mieszko1979 <1377869@gmail.com>
2026-05-29 21:29:40 +0300

    Update points.js

--- points.js
+++ points.js
@@ -2,14 +2,7 @@ var myPoints = [
   { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 124.5, desc: "Палатка", link: "3.gpx", vk: "" },
   { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 85, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
   { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
-  { type: "transfer",   transport: "truck", from: [40.58170501802926, 62.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
-  { type: "transfer",   transport: "car", from: [38.9897194931966, 46.20708448016923], to: [39.207425767655955, 46.391726199499544], dist: 50, title: "Фура", desc: "С водителем познакомился в Иране на границе", link: "", vk: "https://vk.com/wall-171082824_1580" },
-  { type: "transfer",   transport: "train", from: [42.58170501802926, 62.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
-  { type: "transfer",   transport: "plane", from: [41.58170501802926, 62.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
-  { type: "transfer",   transport: "bus", from: [44.58170501802926, 62.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
   { day: "4", lat: 55.687145, lng: 50.407544, title: "03.07.2024", dist: 20, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
-  { type: "transfer",   transport: "train", from: [62.58170501802926, 32.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
-  { type: "transfer",   transport: "plane", from: [48.58170501802926, 67.68008108974445], to: [39.83349834414147, 64.3763952590251], dist: 350, title: "", desc: "Подкинули на автовозе MAN", link: "", vk: "https://vk.com/wall-171082824_1373" },
   { day: "5", lat: 59.687145, lng: 51.407544, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   
  
