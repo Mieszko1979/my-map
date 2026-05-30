3602312e98b0c57b87861900c76475b1cf9a8a12
Mieszko1979 <1377869@gmail.com>
2026-05-30 20:55:39 +0300

    Update points.js

--- points.js
+++ points.js
@@ -1,12 +1,12 @@
 var myPoints = [
   { type: "start", country: "ru", lat: 57.502, lng: 48.316, title: "Старт: Яранск", desc: "Начало большого пути! Погода +25, ветер попутный." },
-  { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 124.5, desc: "Палатка", link: "3.gpx", vk: "" },
-  { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 85, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
-  { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
-  { day: "4", lat: 55.687145, lng: 50.407544, title: "03.07.2024", dist: 20, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
-  { day: "5", lat: 59.687145, lng: 51.407544, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 10, desc: "Палатка", link: "3.gpx", vk: "" },
+  { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 10, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
+  { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "4", lat: 55.687145, lng: 50.407544, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "5", lat: 59.687145, lng: 51.407544, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { type: "border", country: "cn", lat: 51.210, lng: 71.430, title: "Граница", desc: "Въехал в Казахстан. Досмотрели быстро, за 40 минут.", vk: "https://vk.com/wall-123_789" },
-  { day: "6", lat: 19.199528, lng: 101.660691, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "6", lat: 19.199528, lng: 101.660691, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { type: "transfer",   transport: "truck", from: [35.767776, 63.617401], to: [35.58345736621231, 63.32558081544598], dist: 100, title: "Мерседес", desc: "Попутный грузовик с углем", link: "", vk: "https://vk.com/wall-171082824_1474" },
   { type: "transfer",   transport: "car", from: [35.58385242737639, 63.328231284016], to: [34.37794498476834, 62.19187469936118], dist: 100, title: "Такси", desc: "Ночной переезд на такси до Герата за 1500 афгани", link: "", vk: "https://vk.com/wall-171082824_1474" },
   { type: "transfer",   transport: "train", from: [35.58385242737639, 63.328231284016], to: [34.37794498476834, 62.19187469936118], dist: 100, title: "Такси", desc: "Ночной переезд на такси до Герата за 1500 афгани", link: "", vk: "https://vk.com/wall-171082824_1474" },
@@ -14,7 +14,7 @@ var myPoints = [
   { type: "transfer",   transport: "truck", from: [38.9897194931966, 46.20708448016923], to: [39.207425767655955, 46.391726199499544], dist: 100, title: "Фура", desc: "С водителем познакомился в Иране на границе", link: "", vk: "https://vk.com/wall-171082824_1580" },
   { type: "transfer",   transport: "bus", from: [35.58385242737639, 63.328231284016], to: [34.37794498476834, 62.19187469936118], dist: 100, title: "Такси", desc: "Ночной переезд на такси до Герата за 1500 афгани", link: "", vk: "https://vk.com/wall-171082824_1474" },
   { type: "border",   country: "kz", lat: 51.668402668965186, lng: 51.00543464889751, title: "Казахстан - КПП Сырым", desc: "Про переход границы можно почитать в Дзене", dzen: "https://dzen.ru/a/Zp9TCyahOFEfesK2" },
-  { day: "7", lat: 25.199528, lng: 56.660691, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "7", lat: 25.199528, lng: 56.660691, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   
  
 ];
