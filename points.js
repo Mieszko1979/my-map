33da4075db24b2da1914877a958728d6e6f9dc92
Mieszko1979 <1377869@gmail.com>
2026-05-30 15:10:21 +0300

    Update points.js

--- points.js
+++ points.js
@@ -1,9 +1,11 @@
 var myPoints = [
+  { type: "start", country: "ru", lat: 57.502, lng: 48.316, title: "Старт: Яранск", desc: "Начало большого пути! Погода +25, ветер попутный." },
   { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 124.5, desc: "Палатка", link: "3.gpx", vk: "" },
   { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 85, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
   { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { day: "4", lat: 55.687145, lng: 50.407544, title: "03.07.2024", dist: 20, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { day: "5", lat: 59.687145, lng: 51.407544, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { type: "border", country: "la", lat: 51.210, lng: 71.430, title: "Граница", desc: "Въехал в Казахстан. Досмотрели быстро, за 40 минут.", vk: "https://vk.com/wall-123_789" },
   { day: "6", lat: 19.199528, lng: 101.660691, title: "03.07.2024", dist: 100, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   
  
