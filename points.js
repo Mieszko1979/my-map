fe9da36156a9db9bb7e9d45f83b6c7b16b361a67
Mieszko1979 <1377869@gmail.com>
2026-06-04 15:14:34 +0300

    Update points.js

--- points.js
+++ points.js
@@ -8,7 +8,7 @@ var myPoints = [
   { type: "border", country: "cn", lat: 51.210, lng: 71.430, title: "Граница", desc: "Въехал в Казахстан. Досмотрели быстро, за 40 минут.", vk: "https://vk.com/wall-123_789" },
   { day: "6", lat: 19.199528, lng: 101.660691, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { type: "border",   country: "kz", lat: 51.668402668965186, lng: 51.00543464889751, title: "Казахстан - КПП Сырым", desc: "Про переход границы можно почитать в Дзене", dzen: "https://dzen.ru/a/Zp9TCyahOFEfesK2" },
-  { day: "7", lat: 25.199528, lng: 56.660691, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
+  { day: "7", lat: 25.199528, lng: 56.660691, title: "03.07.2024", dist: 10, desc: "Палатка", link: "2026-06-04.gpx", vk: "https://vk.com/wall-171082824_1252" },
   
  
 ];
