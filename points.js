e37adaf2f3469cd52115978b765a153d34838b67
Mieszko1979 <1377869@gmail.com>
2026-06-10 19:32:17 +0300

    Update points.js

--- points.js
+++ points.js
@@ -1,6 +1,6 @@
 var myPoints = [
-  { type: "start", country: "ru", lat: 57.502, lng: 48.316, title: "Старт: Яранск", desc: "Начало большого пути! Погода +25, ветер попутный." },
-  { day: "1", lat: 56.941767, lng: 47.879413, title: "01.07.2024", dist: 10, desc: "Палатка", link: "3.gpx", vk: "" },
+  { type: "start", country: "ru", lat: 57.293493, lng: 47.878047, title: "10.06.2026", desc: "Начало нового велопутешествия." },
+  { day: "1", lat: 56.941767, lng: 47.879413, title: "10.06.2026", dist: 43.84, desc: "Палатка", link: "2026_06_10.gpx", vk: "" },
   { day: "2", lat: 56.133617, lng: 48.330499, title: "02.07.2024", dist: 10, desc: "Палатка", link: "ride-0-2026-05-28-17-34-36.gpx", vk: "https://vk.com/wall-171082824_1252" },
   { day: "3", lat: 55.835278, lng: 48.958066, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
   { day: "4", lat: 55.687145, lng: 50.407544, title: "03.07.2024", dist: 10, desc: "Палатка", link: ["ride-0-2026-05-28-17-34-36.gpx", "3.gpx"], vk: "https://vk.com/wall-171082824_1252" },
